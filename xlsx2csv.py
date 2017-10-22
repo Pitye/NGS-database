80cff71ceb58351a6607d0b756732d29b512a2e4
Pavla Coufalova <coufalova.pavla@gmail.com>
2017-10-23 00:10:42 +0200

    Update xlsx2csv.py

--- xlsx2csv.py
+++ xlsx2csv.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import os
 in_directory = 'Desktop/NGS/xlsx/'
@@ -8,7 +7,7 @@ xlsx_list = os.listdir(in_directory)
 for file in xlsx_list:
     for sheet in sheets:
         path_xlsx = in_directory + file
-        path_csv = out_directory + file[0:-5] + sheet[0] + '.csv'
-        df = pd.read_excel(path_xlsx, sheet, index_col=None, na_values=['NA'])
-        df.to_csv(path_csv)
-print ('hotovo') 
+        path_csv = out_directory + file[0:-5] + '_' + sheet[0] + '.csv'
+        df = pd.read_excel(path_xlsx, sheet, header=None)
+        df.to_csv(path_csv, header=None, index=None)
+print('hotovo')    
